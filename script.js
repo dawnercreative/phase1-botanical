5e543a2148cdc71eaab286de06cda59a37118d7b
Madeliene <madelienestiller@gmail.com>
2024-03-21 11:00:13 +1000

    write comments for functions that need to be created for the plant cards

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const baseUrl = 'https://perenual.com/api/species-list?key=[sk-0XkW65cf040e22c11
 const plantContainer = document.querySelector('.card-container');
 const SearchBox = document.querySelector('.search-box');
 const SearchButton = document.querySelector('.search-button');
+
+
+//Function to create plant cards
+
+
+
+// Function to handle plant button click event
+
+
+
+// Function to display plants on page
+
+
+
+//Function to fetch plants from API
