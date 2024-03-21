a9880e124c67befb5f0811cdddeaec0ce1f4ac99
Madeliene <madelienestiller@gmail.com>
2024-03-21 10:59:12 +1000

    get references to the DOM elements

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const API_KEY = 'sk-0XkW65cf040e22c114200'
 
 const baseUrl = 'https://perenual.com/api/species-list?key=[sk-0XkW65cf040e22c114200]';
 
+//Get references to DOM elements
+const plantContainer = document.querySelector('.card-container');
+const SearchBox = document.querySelector('.search-box');
+const SearchButton = document.querySelector('.search-button');
